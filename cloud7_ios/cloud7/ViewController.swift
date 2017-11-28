87f260b56e42c2eefad45a366ab0c5e207158cb1
evan01 <eknoxmobile@gmail.com>
2017-11-27 22:23:44 -0500

    firebase works!! We can now upload and process data in the cloud, and read that data back.

--- cloud7_ios/cloud7/ViewController.swift
+++ cloud7_ios/cloud7/ViewController.swift
@@ -8,16 +8,29 @@
 
 import UIKit
 import CoreBluetooth
+import Firebase
+import FirebaseAuthUI
+import FirebaseGoogleAuthUI
 
 class ViewController: UIViewController, CBCentralManagerDelegate, CBPeripheralDelegate {
+    
+    @IBOutlet weak var InputField: UITextField!
+    @IBOutlet weak var dataLabel: UILabel!
+    
     var manager:CBCentralManager!
     var peripheral:CBPeripheral!
+    let USERNAME = "USER123"
     let DEVICE_UUID = CBUUID(string: "123A")
     let SERVICE_UUID = CBUUID(string: "a495ff21-c5b1-4b44-b512-1370f02d74de")
     let CHAR_UUID = CBUUID(string: "a495ff21-c5b1-4b44-b512-1370f02d74d1")
     
-    //The following functions happen in a sequence... if everything works.
+    //Firebase
+    var ref: DatabaseReference!
     
+    //Authentication, google
+    let providers: [FUIAuthProvider] = [FUIGoogleAuth()]
+    
+    //The following functions happen in a sequence... if everything works.
     //First make sure that the bluetooth is on, then scan
     func centralManagerDidUpdateState(_ central: CBCentralManager) {
         if central.state == CBManagerState.poweredOn {
@@ -71,16 +84,71 @@ class ViewController: UIViewController, CBCentralManagerDelegate, CBPeripheralDe
             print(characteristic.value?.debugDescription ?? "DEFAULT VAL")
             print(characteristic.value!)
         }
+        
+        //Then upload the data to the database, and process cloud functions!
+    }
+    
+    @IBAction func upload(_ sender: Any) {
+        uploadToFirebase()
+    }
+    
+    
+    func uploadToFirebase(){
+        let d = InputField.text;
+        let email = "eknoxmobile@gmail.com"
+        let password = "cloud7"
+        Auth.auth().signIn(withEmail: email, password: password) { (user, error) in
+            self.ref.child("audio").child("USER123").updateChildValues(["data2": d ?? ""]);
+            print("Uploaded data to database")
+            do {
+                //Then read the return value...
+                sleep(3)
+                self.ref.child("audio").child("USER123").observeSingleEvent(of: .value, with: { (snapshot) in
+                    // Get user value
+                    let value = snapshot.value as? NSDictionary
+                    let procVal = value?["return_value"] as? String ?? ""
+                    self.dataLabel.text = procVal
+                }) { (error) in
+                    print(error.localizedDescription)
+                }
+                try Auth.auth().signOut()
+            } catch let signOutError as NSError {
+                print("Error signing out")
+                print(signOutError)
+            }
+        }
+        
+    }
+    
+    func retrieveDataFromFirebase() -> Bool {
+        return true;
     }
     
     func centralManager(_ central: CBCentralManager, didDisconnectPeripheral peripheral: CBPeripheral, error: Error?) {
         central.scanForPeripherals(withServices: nil, options: nil)
     }
-
+    
+    //This will handle the google sign in
+    func application(_ app: UIApplication, open url: URL,
+                     options: [UIApplicationOpenURLOptionsKey : Any]) -> Bool {
+        let sourceApplication = options[UIApplicationOpenURLOptionsKey.sourceApplication] as! String?
+        if FUIAuth.defaultAuthUI()?.handleOpen(url, sourceApplication: sourceApplication) ?? false {
+            return true
+        }
+        return false
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.manager = CBCentralManager(delegate: self, queue: nil)
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        //Bluetooth configuration
+//        self.manager = CBCentralManager(delegate: self, queue: nil)
+        
+        //Firebase configuration
+        self.ref = Database.database().reference()
+        
+        //Upload user data to firebase
+        uploadToFirebase()
     }
 
     override func didReceiveMemoryWarning() {
--- cloud7_ios/firebase/index.js
+++ cloud7_ios/firebase/index.js
@@ -1,7 +1,24 @@
 // The Cloud Functions for Firebase SDK to create Cloud Functions and setup triggers.
 const functions = require('firebase-functions');
-
-// The Firebase Admin SDK to access the Firebase Realtime Database. 
 const admin = require('firebase-admin');
 admin.initializeApp(functions.config().firebase);
 
+
+//Takes our bluetooth data and adds it to the realtime firebase database
+exports.uploadAudio = functions.https.onRequest((req, res) => {
+  const original = req.query.text;// Grab the text parameter. APIENDPOINT?text=whateveryouwant
+  // Push the new message into the Realtime Database using the Firebase Admin SDK.
+});
+
+// exports.writeAudio = functions.database.onWrite(('/audio/{pushId}/write'))
+
+// When new data is uploaded to the realtime database... then we process/return it.
+exports.processAudio = functions.database.ref('/audio/USER123/data2')
+    .onUpdate(event => {
+      const data = event.data.val();// Grab the current value of what was written to the Realtime Database.
+      console.log('Audio: '+ data);
+      const retVal = "The data was: " + data;
+      return event.data.ref.parent.child('return_value').set(retVal).then(snapshot => {
+          res.redirect(303,snapshot.ref);
+      });
+    });
